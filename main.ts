571d990bd5c4ebd282fbb9a2ab6604f9787803c0
Christophe CHAUDELET <cho934@users.noreply.github.com>
2025-05-22 21:46:42 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString(control.deviceName())
+})
+input.onButtonPressed(Button.B, function () {
+    mergedGraph = !(mergedGraph)
+})
+let mergedGraph = false
+mergedGraph = true
+bluetooth.startUartService()
+basic.forever(function () {
+    if (mergedGraph) {
+        bluetooth.uartWriteValue("a.x", input.acceleration(Dimension.X))
+        bluetooth.uartWriteValue("a.y", input.acceleration(Dimension.Y))
+        bluetooth.uartWriteValue("a.z", input.acceleration(Dimension.Z))
+    } else {
+        bluetooth.uartWriteValue("x", input.acceleration(Dimension.X))
+        bluetooth.uartWriteValue("y", input.acceleration(Dimension.Y))
+        bluetooth.uartWriteValue("z", input.acceleration(Dimension.Z))
+    }
+})
